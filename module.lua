2438bd7c5fffcd7ab4170a37c91be133a161b4f5
anotherrandomaccount2 <riblolox@gmail.com>
2025-03-07 22:34:59 -0600

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,1 +1,27 @@
+local module = {}
+module.GuiCreate = {}
+local guiCreator = module.GuiCreate
 
+--
+
+function getService(a)
+    return game:GetService(a)
+end
+
+
+
+--
+
+local UIS = getService("UserInputService")
+
+local isExecuter, platform = pcall(function()
+    return UIS:GetPlatform()
+end)
+
+--
+
+function module:ExploitHub()
+    
+end
+
+return module
