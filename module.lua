184bd5555b31bbd779f8dd6ab4982431ca41c1f5
anotherrandomaccount2 <riblolox@gmail.com>
2025-03-08 00:05:44 -0600

    Update module.lua

--- module.lua
+++ module.lua
@@ -16,9 +16,17 @@ local isExecuter, platform = pcall(function()
     return UIS:GetPlatform()
 end)
 
+local guiHost = isExecuter and game.CoreGui or game.Players.LocalPlayer.PlayerGui
+
 --
 
 function module:ExploitHub()
+    if guiHost:FindFirstChild("SigmaHub")
+end
+
+--
+
+function guiCreator.ExploitHub()
     
 end
 
